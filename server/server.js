d2e2ca9be6e917edb9d2491db59edbc4a45e7c34
Kedar-sonavani <Kedar.sonavani@gmail.com>
2026-04-16 00:24:20 +0530

    Deploy: Fix CORS headers for admin and add profile section

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use(cors({
 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-admin-secret'],
     optionsSuccessStatus: 200
 }));
 
